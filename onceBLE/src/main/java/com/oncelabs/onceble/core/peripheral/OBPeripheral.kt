798733a2f4390f4c919e1580adf0ca642f1e3721
JoeBakalor <jmbakalor@gmail.com>
2021-02-05 18:04:50 -0500

    expose connection state

--- onceBLE/src/main/java/com/oncelabs/onceble/core/peripheral/OBPeripheral.kt
+++ onceBLE/src/main/java/com/oncelabs/onceble/core/peripheral/OBPeripheral.kt
@@ -40,7 +40,16 @@ open class OBPeripheral<G: OBGatt>(device: BluetoothDevice? = null, scanResult: 
     private var pendingGATTRequest: OBGattRequest? = null
 
     // Status flags
+    var state: LiveData<ConnectionState> = MutableLiveData()
+        get() = _state
+        private set
+    private var _state:MutableLiveData<ConnectionState> = MutableLiveData()
+
     private var connectionState: ConnectionState = ConnectionState.disconnected
+        set(value) {
+            _state.postValue(value)
+            field = value
+        }
 
     // Handlers
     private var serviceDiscoveryHandler: ServiceDiscoveryHandler? = null
